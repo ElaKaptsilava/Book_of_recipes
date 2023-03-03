e9bc94a690c99bd4fa7ffda3ba2c0df608e9949f
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-03-03 09:09:42 +0100

    Creating a manager menu

--- book_of_recipes.py
+++ book_of_recipes.py
@@ -1,8 +1,22 @@
 class Manager:
-    pass
+    def show(self):
+        menu = Menu()
+        menu.show_menu()
 
 class Menu(Manager):
-    pass
+    def show_menu(self):
+        manager_menu = '''1. Add a new recipe
+        2. Show all recipes
+        3. Would you like to return to the choice?
+        '''
+        self.show_choice()
+
+    def show_choice(self):
+        user_choice = str(input())
+        self.make_choice(choice=user_choice)
+
+    def make_choice(self, choice):
+        pass
 
 class Recipes:
     pass
