0ae505b6566ae0c0a9fb57ac9bcff60e72c05adf
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-03-03 10:10:19 +0100

    edit class manager

--- book_of_recipes.py
+++ book_of_recipes.py
@@ -3,26 +3,49 @@ class Manager:
         menu = Menu()
         menu.show_menu()
 
+
 class Menu(Manager):
     def show_menu(self):
-        manager_menu = '''1. Add a new recipe
-        2. Show all recipes
-        3. Would you like to return to the choice?
-        '''
-        self.show_choice()
+        manager_menu = '1. Add a new recipe\n2. Show all recipes\n3. Would you like to return to the choice?'
+        print(manager_menu)
+        self.make_choice()
 
-    def show_choice(self):
-        user_choice = str(input())
-        self.make_choice(choice=user_choice)
+    def make_choice(self):
+        while True:
+            user_choice = str(input())
+            self.conditions_choice(choice=user_choice)
 
-    def make_choice(self, choice):
-        pass
+    def conditions_choice(self, choice):
+        recipes = Recipes()
+        if choice == 1:
+            pass
+        elif choice == 2:
+            recipes.show_recipes()
+
 
 class Recipes:
-    pass
+    def add_ingridients(self):
+        pass
+
+    def add_Instruction(self):
+        pass
+
+    def show_recipes(self):
+        pass
+
 
 class Ingredients(Recipes):
     pass
 
-class Instrukcja(Recipes):
+
+class Instruction(Recipes):
     pass
+
+
+def main():
+    manager = Manager()
+    manager.show()
+
+
+if __name__ == '__main__':
+    main()
