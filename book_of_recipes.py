343afb74306c7e50f076732a04f3c1686875470f
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-03-06 14:37:38 +0100

    created class recipe

--- book_of_recipes.py
+++ book_of_recipes.py
@@ -1,57 +1,78 @@
 class Manager:
     def show(self):
         menu = Menu()
-        menu.show_menu()
+        menu.make_choice()
 
 
 class Menu(Manager):
-    def show_menu(self):
-        manager_menu = '1. Add a new recipe\n2. Show all recipes\n3. Would you like to return to the choice?'
-        print(manager_menu)
-        self.make_choice()
+    def __init__(self):
+        self.recipes = Recipe()
 
     def make_choice(self):
+        manager_menu = '1. Add a new recipe\n2. Show all recipes\n3. Would you like to return to the choice?'
+
         while True:
+            print(manager_menu)
             user_choice = input()
+            if not user_choice:
+                break
             self.conditions_choice(choice=user_choice)
 
     def conditions_choice(self, choice):
-        recipes = Recipe()
         if choice == '1':
-            recipes.show_menu()
+            self.recipes.recipe_name()
         elif choice == '2':
-            pass
+            self.recipes.show_recipes()
 
 
-class Recipe:
-    recipes = {}
+class Recipe(Manager):
+    recipes = []
+
     def __init__(self):
         self.name = []
-        self.recipe_ingridients = []
-        self.recipe_instruction = []
+        self.ingridients = []
+        self.instruction = []
 
-    def show_menu(self):
+    def __repr__(self):
+        return f'{self.name}:\n {self.ingridients}\n\n{self.instruction}'
+
+    def __str__(self):
+        return f'{self.name}:\n {self.ingridients}\n\n{self.instruction}'
+
+    def recipe_name(self):
         print('Enter recipe name: ')
-        enter_name_recipe = input()
-        self.recipe_name(recipe_name=enter_name_recipe)
-
-    def recipe_name(self, recipe_name):
-        self.name.append(recipe_name)
+        recipe_name = input()
+        self.name = recipe_name
         print(f'Enter the ingredients needed for cooking "{recipe_name}":')
-        enter_ingridients = input()
-        self.add_recipe_ingridients(ingridients=enter_ingridients)
+        self.recipe_ingridients()
 
-    def add_recipe_ingridients(self, ingridients):
-        self.recipe_ingridients.append(ingridients)
+    def recipe_ingridients(self):
+        while True:
+            enter_ingridients = input()
+            if not enter_ingridients:
+                break
+            self.ingridients = enter_ingridients.split()
         print(f'Enter the instructions:')
-        enter_instruction = input()
-        self.add_recipe_instruction(instructions=enter_instruction)
+        self.recipe_instruction()
 
-    def add_recipe_instruction(self, instructions):
+    def recipe_instruction(self):
+        while True:
+            enter_instruction = input()
+            if not enter_instruction:
+                break
+            self.instruction = enter_instruction
+
+    def make_recipe_book(self):
+        #self.recipes[self.name] = {'Ingridients': self.ingridients, 'Instructions': self.instruction}
         pass
 
     def show_recipes(self):
-        pass
+        for name in self.name:
+            print(name)
+        for ingridients in self.ingridients:
+            print(ingridients)
+        for number, instruction in enumerate(self.instruction, 1):
+            print(instruction)
 
 
 def main():
